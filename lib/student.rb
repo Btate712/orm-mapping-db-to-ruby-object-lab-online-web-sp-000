b0e6b0322848e81f658183683ab73bde43b9de61
Robert Tate <btate712@gmail.com>
2019-04-11 00:13:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
     students.map { |row| self.new_from_db(row) }
   end
 
-  def self.students_below_grade_12
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade < 12
